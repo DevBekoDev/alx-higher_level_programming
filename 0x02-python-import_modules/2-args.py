0532877c7a9e6a4cfe5ef1912779a5a26cda3b03
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-11-04 23:42:00 -0700

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ argv = sys.argv
 if len(argv) == 1:
     print("0 arguments.")
 else:
-    print("{} arguments:".format(len(argv) - 1))
+    if len(argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(argv) - 1))
 for i in range(1, len(argv)):
     print("{:d}: {:s}".format(i, argv[i]))
     
