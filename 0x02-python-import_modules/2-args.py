fee7e4f6c3df9ba02bc104b133a8c66325c136cb
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-11-04 23:43:55 -0700

    modifed the output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 
-import sys
-argv = sys.argv
-if len(argv) == 1:
-    print("0 arguments.")
-else:
-    if len(argv) == 2:
-        print("1 argument:")
+    import sys
+    argv = sys.argv
+    if len(argv) == 1:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(len(argv) - 1))
-for i in range(1, len(argv)):
-    print("{:d}: {:s}".format(i, argv[i]))
+        if len(argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
     
 
