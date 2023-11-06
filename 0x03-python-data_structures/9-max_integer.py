a726a199f486cfeaace344d51e98345a9ab7cb53
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-11-06 05:04:38 -0800

    took different approach: git push

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    s = 0
-    if len(my_list) <= 0:
-        s = None
-        return s
+    if my_list:
+        my_list.sort()
+        return (my_list[-1])
     else:
-        for i in range(len(my_list)):
-            if my_list[i] >= s:
-                s = my_list[i]
-    return s
-
+        return None
