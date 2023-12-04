2fecfd8a9672cdb16404add1ae40a9843d9d5c01
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-12-04 10:36:48 -0800

    defined a puplic instance

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""square class
+
+    defining class args
+"""
+
+
+class Square:
+    """class square
+
+    defining class args
+
+    """
+
+    def __init__(self, size=0):
+        """square has a size
+
+        the size of the square
+
+        """
+
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """area of a square
+
+        area = size ** 2
+        """
+
+        return (self.__size ** 2)
