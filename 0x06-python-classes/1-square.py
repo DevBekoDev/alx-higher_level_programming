32e9f4a62a72046e7d1fc8ee5f0b19063ff65354
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-12-04 10:21:07 -0800

    added size attribute to class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""square class
+
+    defining class args
+"""
+
+
+class Square:
+    """class square
+
+    defining class args
+    
+    """
+
+
+    def __init__(self, size):
+        """square has a size
+
+        the size of the square
+
+        """
+
+
+        self.__size = size
+
