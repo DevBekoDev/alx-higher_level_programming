f81356fab3eb7a98e5d73d76de16f05fcb3c45d0
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-12-06 09:11:52 -0800

    tset

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,5 +12,4 @@ def inherits_from(obj, a_class):
     if type(obj) is a_class:
         return False
     else:
-        if isinstance(obj, a_class):
-            return True
+        return isinstance(obj, a_class)
