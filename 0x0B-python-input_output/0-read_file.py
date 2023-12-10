313774438561df4cc2dc557cf03d8c3b65a305de
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-12-10 14:38:57 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+I/O python
+"""
 
 
 def read_file(filename=""):
+    """
+    func to read a file
+    """
+
     with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+        for line in f:
+            print(line, end="")
