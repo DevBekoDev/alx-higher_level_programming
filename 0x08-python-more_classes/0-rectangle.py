644d71f771491cb0eb5fdc9d3e718092f9e9d2b2
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-12-10 09:17:13 -0800

    setters and getter

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -7,4 +7,33 @@ Rectangle Class
 class Rectangle:
     """ Rectangle Class"""
 
-    pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.width
+
+    @setter.width
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.width = value
+
+    @property
+    def height(self):
+        return self.height
+
+    @setter.height
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.height = value
+
