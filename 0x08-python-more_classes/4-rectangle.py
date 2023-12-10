6890621d3ea46a84d5589826fcfc1f9564018602
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-12-10 10:17:28 -0800

    test

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -97,4 +97,4 @@ class Rectangle:
         String represntation for the rectangle
         """
 
-        return("Rectangle({}, {})".format(self.__width, self.__height))
+        return("Rectangle({:d}, {:d})".format(self.__width, self.__height))
