4e4fafe0437b940a9c76211d3c4a3ff2ffe7bd5a
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2024-02-12 11:11:58 -0800

    capital X

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,6 +6,6 @@ if (process.argv.length === 2) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log('x'.repeat(parseInt(process.argv[2])));
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
