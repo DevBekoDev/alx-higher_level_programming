dcd017d762221a5d742d98705587cb6d7a6e19e1
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2024-02-18 01:41:18 -0800

    lsiting states using python orm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Get all states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    curr = db.cursor()
+    curr.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = curr.fetchall()
+
+    for row in rows:
+        print(row)
+    curr.close()
+    db.close()
