016c2a3e50c27c9d1f303fbe742cfe0f44c1cff3
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2024-02-18 04:01:20 -0800

    first state model using orm

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" using ORM"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    class of the states table
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
