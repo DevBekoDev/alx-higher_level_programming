a344e2f1bc8319c4cb6308d3bebc3be520dacd25
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2024-02-18 18:50:10 -0800

    print format

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     cities = session.query(City, State).\
         filter(City.state_id == State.id).all()
     for city, state in cities:
-        print("{}: ({} {})".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.commit()
     session.close()
