41f415d05c7f2ef5892e6733cf5d8a131f0880a3
Abubakr Nasir <abubakr.ezalden.nasir@gmail.com>
2024-03-26 16:13:18 -0700

    fixed semistandard issue

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,10 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
 request(url, function (err, result, body) {
+  if (err) {
+    console.log(err);
+  } else {
     body = JSON.parse(body);
     console.log(body.title);
+  }
 });
