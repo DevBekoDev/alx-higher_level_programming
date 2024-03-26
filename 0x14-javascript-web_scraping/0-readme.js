feba7b9e6b616b3e698fb33e20a7cf82c7100e3c
Abubakr Nasir <abubakr.ezalden.nasir@gmail.com>
2024-03-26 15:45:41 -0700

    reading json file with node js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], {enoding: "utf-8"}, function (err,data)
+	{
+		if (err) 
+		{
+			console.log(err);
+		}
+		else
+		{
+			process.stdout.write(data);
+		}
+	});
