02b4b5d50a580cd9f568fcdb2c7d1ffb999552c0
Abubakr Nasir <abubakr.ezalden.nasir@gmail.com>
2024-03-26 16:45:30 -0700

    bingo

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -12,13 +12,13 @@ request(process.argv[2], function (err, _result, body) {
     for (let i = 0; i < body.length; ++i) {
       // Extract the userId and completed properties from each element
       const userId = body[i].userId;
-      const done = body[i].done;
+      const completed = body[i].completed;
 
-      if (done && !doneByUser[userId]) {
+      if (completed && !doneByUser[userId]) {
         doneByUser[userId] = 0;
       }
 
-      if (done) ++doneByUser[userId];
+      if (completed) ++doneByUser[userId];
     }
     console.log(doneByUser);
   }
